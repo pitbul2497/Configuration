302c6ac8cb45c6826308623ce4c0bae5774074db
Borzhikov Temirlan <pitshow277@gmail.com>
2022-05-15 01:03:31 +0700

    Add

---
+++ scripts/processElementData.js
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["{002674F9-6511-4D15-8623-B761D8C48986}Activity"]="./../../core.base_rup/capabilitypatterns/resources/analysis_design_discipline_49725AC4_49725ac4_Activity.jpeg"
+contentPage.processPage.imageFiles["{F2AD342D-0F3B-4E19-A351-75ECDCB806F5}Activity"]="./../../core.base_rup/capabilitypatterns/resources/implementation_8FF0AAD4_8ff0aad4_Activity.jpeg"
+contentPage.processPage.imageFiles["{35359DDF-6361-43E5-8B1B-18D204DA8CFF}Activity"]="./../../core.base_rup/capabilitypatterns/resources/test_7EF99F54_7ef99f54_Activity.jpeg"
+contentPage.processPage.imageFiles["{EA0C1D32-E39A-4D96-B956-E178DB776A96}Activity"]="./../../core.base_rup/capabilitypatterns/resources/project_management_FC6669AF_fc6669af_Activity.jpeg"
+contentPage.processPage.imageFiles["{C85C9CC0-AD92-46C9-85BA-4C0672D6571E}Activity"]="./../../core.base_rup/capabilitypatterns/resources/configuration_change_management_5C81A290_5c81a290_Activity.jpeg"
+contentPage.processPage.imageFiles["{38A9C609-9A59-4D03-B835-AA84A716E626}Activity"]="./../../core.base_rup/capabilitypatterns/resources/requirements_4EE062EE_4ee062ee_Activity.jpeg"
---
+++ theme/treeNodes.js
@@ -0,0 +1,5 @@
+theApp.nav.treeViews = [
+{id: "view:_LVCagP5WEdmAzesbYywanQ", objectId: "_LVCagP5WEdmAzesbYywanQ", label: "Наборы ролей", src: "theme/role_sets_5CA781CC.json", isDefault: false},
+{id: "view:3.2329841703462285E-305", objectId: "3.2329841703462285E-305", label: "Начало работы", src: "theme/getting_started_E6279909.json", isDefault: false},
+{id: "view:_FCx1oN7CEdmsEI4YDGX2ag", objectId: "_FCx1oN7CEdmsEI4YDGX2ag", label: "Коллектив", src: "theme/team_A2C7CEF1.json", isDefault: true}];
+
